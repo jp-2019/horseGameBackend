3028217d769df83b25df22ed79121f4e003cbbed
JP <49924656+jp-2019@users.noreply.github.com>
2019-08-29 09:34:13 -0400

    Comment out headers in signup.php

--- signup.php
+++ signup.php
@@ -1,8 +1,8 @@
 <?php
 
-header('Access-Control-Allow-Origin= *');
-header( 'Access-Control-Allow-Headers= X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method, Authorization' );
-header( 'Access-Control-Allow-Methods= GET, POST, OPTIONS, PUT, DELETE' );
+// header('Access-Control-Allow-Origin= *');
+// header( 'Access-Control-Allow-Headers= X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method, Authorization' );
+// header( 'Access-Control-Allow-Methods= GET, POST, OPTIONS, PUT, DELETE' );
 
 $servername =  localhost ;
 $username =  root ;
